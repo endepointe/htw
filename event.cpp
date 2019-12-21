034bae052919d5aac0dafa6419f9fc518ff560af
Ende <30739491+endepointe@users.noreply.github.com>
2019-12-20 22:32:43 -0800

    Update event.cpp

--- event.cpp
+++ event.cpp
@@ -1,9 +1,3 @@
-/****************************************************************************
- * Filename: event.cpp
- * Author: Alvin Johns
- * Date: Nov 21, 2019
- * Desc: sets default constructor for each event type
- ***************************************************************************/
 
 #include "event.h"
 #include <string>
