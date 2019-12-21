74bc9de9ac0c76aa5bbe9f06893ed5f95a0c7810
Ende <30739491+endepointe@users.noreply.github.com>
2019-12-20 22:31:42 -0800

    Update bat.cpp

--- bat.cpp
+++ bat.cpp
@@ -1,11 +1,4 @@
-/*****************************************************************************
- * Filename: bat.cpp
- * Author: Alvin Johns
- * Date: Nov 20, 2019
- * Desc: Class to hold the operations of the bat
- * Input: none
- * Output: event string
- ****************************************************************************/
+
 #include "event.h"
 #include "bat.h"
 #include <string>
