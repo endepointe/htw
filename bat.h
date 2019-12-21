ad979d4c4933a957885c5c21465aa088e17f1bbb
Ende <30739491+endepointe@users.noreply.github.com>
2019-12-20 22:32:10 -0800

    Update bat.h

--- bat.h
+++ bat.h
@@ -1,11 +1,4 @@
-/*****************************************************************************
- * Filename: bat.h
- * Author: Alvin Johns
- * Date: Nov 20, 2019
- * Desc: Class to hold the operations of the bat
- * Input: none
- * Output: event string
- ****************************************************************************/
+
 #ifndef BAT_H
 #define BAT_H
 
