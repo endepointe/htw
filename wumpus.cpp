7376380785faa6b51b566fa6767e3ad58020d09b
Ende <30739491+endepointe@users.noreply.github.com>
2020-03-28 15:40:56 -0700

    Update wumpus.cpp

--- wumpus.cpp
+++ wumpus.cpp
@@ -1,11 +1,4 @@
-/*****************************************************************************
- * Filename: wumpus.cpp
- * Author: Alvin Johns
- * Date: Nov 20, 2019
- * Desc: Class to hold the operations of the wumpus
- * Input: none
- * Output: event string
- ****************************************************************************/
+
 #include "event.h"
 #include "wumpus.h"
 #include <string>
