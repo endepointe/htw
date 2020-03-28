b486eeeaa7cfb1a9758657a08f20074bbf9be8a4
Ende <30739491+endepointe@users.noreply.github.com>
2020-03-28 15:40:37 -0700

    Update rope.h

--- rope.h
+++ rope.h
@@ -1,11 +1,4 @@
-/*****************************************************************************
- * Filename: rope.h
- * Author: Alvin Johns
- * Date: Nov 20, 2019
- * Desc: Class to hold the operations of the rope
- * Input: none
- * Output: event string
- ****************************************************************************/
+
 #ifndef ROPE_H
 #define ROPE_H
 
